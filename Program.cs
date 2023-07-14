c8afe75cbe09294373f12b40ffd6d8f484e23817
Frédéric Jacques <gary.bob88@gmail.com>
2023-07-14 16:10:29 -0400

    edit

--- Program.cs
+++ Program.cs
@@ -20,43 +20,58 @@ class Program
         string path = @"C:\Users\jacqu\Downloads\Default14_07_2023_11_27_06.xlsx";
 
         Console.WriteLine("Enter the name of the sheet you want to select: ");
-        string? sheetName = Console.ReadLine();
+        string? sheetName = Console.ReadLine() ?? "";
 
         // Open the file
         using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(path, false))
         {
             var feuille = SeletionneFeuille(spreadsheetDocument, sheetName);
 
+            var rows = feuille.Descendants<Row>().ToList();
+            Console.WriteLine("Sheet number of rows: " + rows.Count);
+
             // From the sheet loop line by line and print the value of the cell
-            foreach (Row row in feuille.Descendants<Row>())
+            foreach (Row row in rows)
             {
                 foreach (Cell cell in row.Descendants<Cell>())
                 {
-                    Console.WriteLine(cell.CellReference + " " + cell.CellValue.Text);
+                    Console.WriteLine(cell.CellReference + " " + GetCellValue(cell, spreadsheetDocument));
                 }
             }
         }
     }
 
-    private static Sheet SeletionneFeuille(SpreadsheetDocument spreadsheetDocument, string sheetName)
+    private static Worksheet SeletionneFeuille(SpreadsheetDocument spreadsheetDocument, string sheetName)
     {
         // Get the workbook
-        WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
+        WorkbookPart wbPart = spreadsheetDocument.WorkbookPart;
 
         // Get the sheets
-        Sheets sheets = workbookPart.Workbook.Sheets;
+        Sheets sheets = wbPart.Workbook.Sheets;
 
         // Loop through the sheets
         foreach (Sheet sheet in sheets)
         {
             // Print the name of the sheet
-            Console.WriteLine("Sheet name: " + sheetName);
+            Console.WriteLine("Sheet name: " + sheet.Name);
 
-            if (sheetName == sheet.Name) {
-                return sheet;
+            if (string.Equals(sheetName, sheet.Name, StringComparison.OrdinalIgnoreCase)) {
+                WorksheetPart wsPart =
+                    (WorksheetPart)(wbPart.GetPartById(sheet.Id));
+                Worksheet ws = wsPart.Worksheet;
+                return ws;
             }
         }
 
         throw new KeyNotFoundException($"Sheet {sheetName} not found");
+    }
+
+    private static string GetCellValue(Cell cell, SpreadsheetDocument originalWorksheet) {
+        if (cell.DataType != null && cell.DataType == CellValues.SharedString) {
+            var stringTable = originalWorksheet.WorkbookPart.SharedStringTablePart.SharedStringTable;
+            return stringTable.ElementAt(int.Parse(cell.InnerText)).InnerText;
+        } else {
+            return cell.InnerText;
+        }
     }
 }
